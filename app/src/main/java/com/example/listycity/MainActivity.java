c1a00faa4db384bfaf34e1bfd9e06c2ce13bc268
Kevin Chan <140776786+kevindkchan@users.noreply.github.com>
2025-01-15 20:11:15 -0700

    Update

--- app/src/main/java/com/example/listycity/MainActivity.java
+++ app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.listycity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -18,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     ListView cityList;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
+    Button addCityButton, deleteCityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         cityList = findViewById(R.id.city_list);
+        addCityButton = findViewById(R.id.add_city_button);
+        deleteCityButton = findViewById(R.id.delete_city_button);
 
         String []cities = {"Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin", "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"};
 
@@ -34,5 +42,47 @@ public class MainActivity extends AppCompatActivity {
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
 
+        addCityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addCity();
+            }
+        });
+
+        deleteCityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteCity();
+            }
+        });
+
     }
+
+    private void addCity() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        final EditText input = new EditText(this);
+        builder.setView(input);
+
+        builder.setPositiveButton("CONFIRM", (dialog, which) -> {
+            String cityName = input.getText().toString().trim();
+            if (!cityName.isEmpty()) {
+                dataList.add(cityName);
+                cityAdapter.notifyDataSetChanged();
+            }
+        });
+
+        builder.setNegativeButton("CANCEL", (dialog, which) -> dialog.cancel());
+
+        builder.show();
+    }
+
+    private void deleteCity() {
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            dataList.remove(position);
+            cityAdapter.notifyDataSetChanged();
+            cityList.setOnItemClickListener(null);
+        });
+    }
+
 }
